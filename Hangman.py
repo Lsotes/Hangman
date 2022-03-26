52175a71214b639bf68e8b2150046b679709aff1
Lisandro Sotes <lsotesws@gmail.com>
2022-03-26 11:43:00 -0300

    Creación de la lista con espacios blancos

--- Hangman.py
+++ Hangman.py
@@ -1,19 +1,30 @@
+# Modulos importados
 import random
 import os
 
+# Creación y enumeración de la base de datos de palabras
 palabra = []
 with open("./data.txt","r",encoding="utf-8") as f:
     for line in f:
         palabra.append(str(line))
-enumerada = enumerate(palabra)
+enumerada = dict(enumerate(palabra,0))
+
+# Desarrollo del elector de palabras aleatorio
+cuenta = len(enumerada)
+elegida = str(enumerada.get(random.randint(0,cuenta)))
+
+intentos = 7
 
 def run():
     print("¡Adivina la palabra!")
-    print(enumerada)
+    resultado = []
+    
+    for i in range(intentos):
+        letra = str(input("Ingresa una letra y presiona enter: "))
+        if letra == elegida[range(0,len(elegida)+1)]:
+            resultado.append(letra)
 
-
-    letra = str(input("Ingresa una letra: "))
-
+    
 
 
 
